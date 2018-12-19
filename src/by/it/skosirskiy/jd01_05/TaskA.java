3358d61d5c469125e46299445b9d7bcab6878ccb
skosirskiy <x987110@gmail.com>
2018-12-19 23:48:15 +0300

    lesson 1-4

---
+++ src/by/it/skosirskiy/jd01_05/TaskA.java
@@ -0,0 +1,17 @@
+package by.it.skosirskiy.jd01_05;
+
+public class TaskA {
+    public static void main(String[] args) {
+        step1();
+        step2();
+        step3();
+    }
+
+    private static void step1() {
+        float a = (float) 756.13;
+    }
+    private static void step2() {
+    }
+    private static void step3() {
+    }
+}
