cb57bda0d69cf77d22e6321dc89317773e363bdd
skosirskiy <x987110@gmail.com>
2018-12-19 22:56:05 +0300

    lesson 1-4

--- src/by/it/skosirskiy/jd01_04/TaskB.java
+++ src/by/it/skosirskiy/jd01_04/TaskB.java
@@ -10,22 +10,22 @@ public class TaskB {
         int kol_chel = scanner.nextInt();
         float itogo = 0;
         float srednee=0;
-        Scanner scanner2= new Scanner(System.in);
+
         int [][] arrayZp= new int[kol_chel][4];
 
-        Scanner sc= new Scanner(System.in);
+
 
         System.out.println("Введите фамилии");
         String []arr= new String[kol_chel];
         for (int i = 0; i < kol_chel; i++) {
-            arr[i]= sc.next();
+            arr[i]= scanner.next();
         }
 
 
         for (int i = 0; i < kol_chel; i++) {
             System.out.printf("%15s %15s \n", "Введите заплату ", arr[i]);
             for (int j = 0; j <4 ; j++) {
-                arrayZp[i][j]=scanner2.nextInt();
+                arrayZp[i][j]=scanner.nextInt();
             }
         }
 
@@ -54,8 +54,8 @@ public class TaskB {
         System.out.println();
         System.out.println("-------------------------------------------------------------------------------------------------");
         srednee=itogo/(kol_chel*4);
-        System.out.printf("\n %-10s %7.0f \n %-10s %5.3f", "итого:",  itogo, "среднее", srednee);
-    //System.out.printf("\n itogo: %7d \n srednee: %5.4f", itogo, srednee);
+        System.out.printf("\n %-10s %7.0f \n %-10s %5.5f", "итого:",  itogo, "среднее", srednee);
+
 
 
 
