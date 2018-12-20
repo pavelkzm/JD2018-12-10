0c54825b3504b50dad16330b55ebd49e14021f69
skosirskiy <x987110@gmail.com>
2018-12-20 14:41:39 +0300

    lesson 1-4

--- src/by/it/skosirskiy/jd01_05/TaskA.java
+++ src/by/it/skosirskiy/jd01_05/TaskA.java
@@ -1,5 +1,7 @@
 package by.it.skosirskiy.jd01_05;
 
+import static java.lang.Math.*;
+
 public class TaskA {
     public static void main(String[] args) {
         step1();
@@ -8,10 +10,20 @@ public class TaskA {
     }
 
     private static void step1() {
-        float a = (float) 756.13;
+        double a =  756.13;
+        double x =  0.3;
+        double z;
+
+        z= cos(pow((x*x+PI/6),5))-sqrt(x*(pow(a,3)))-log(abs((a-1.12*x)/4));
+        System.out.println(z);
     }
-    private static void step2() {
+    private static void step2(){
+        double a= 1.21;
+        double b=0.371;
+        double y=tan((a+b)*(a+b))-pow((a+1.5), 1/3.0)+a*pow(b, 5)-(b/log(a*a));
+        System.out.println(y);
     }
     private static void step3() {
+
     }
 }
---
+++ src/by/it/skosirskiy/jd01_05/TaskB.java
@@ -0,0 +1,28 @@
+package by.it.skosirskiy.jd01_05;
+import static java.lang.Math.*;
+public class TaskB {
+    public static void main(String[] args) {
+        step1();
+        step2();
+    }
+
+    private static void step1() {
+        double sum=0;
+        int countMas=6;
+        double a=0;
+
+
+   //     do {
+            //for (int i = 0; i < 6; i++) {
+          //      int x=0;
+        //        sum=pow(7,a)-cos(x);
+      //      }
+    //
+     //   }while ((0<=a)&&(a<=2));
+     //   System.out.println(sum);
+
+    }
+    private static void step2() {
+    }
+
+}
