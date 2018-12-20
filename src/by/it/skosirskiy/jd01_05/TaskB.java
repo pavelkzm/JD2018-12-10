ad996acf6a88e5f8684c671215883bba3bd600f1
skosirskiy <x987110@gmail.com>
2018-12-20 22:20:53 +0300

    lesson 1-4

--- src/by/it/skosirskiy/jd01_05/TaskB.java
+++ src/by/it/skosirskiy/jd01_05/TaskB.java
@@ -2,27 +2,56 @@ package by.it.skosirskiy.jd01_05;
 import static java.lang.Math.*;
 public class TaskB {
     public static void main(String[] args) {
-        step1();
+      step1();
         step2();
     }
 
     private static void step1() {
-        double sum=0;
-        int countMas=6;
+
         double a=0;
+        do {
+            double sum=0;
+            double y=0;
+            for (int i = 1; i < 7; i++) {
 
-
-   //     do {
-            //for (int i = 0; i < 6; i++) {
-          //      int x=0;
-        //        sum=pow(7,a)-cos(x);
-      //      }
-    //
-     //   }while ((0<=a)&&(a<=2));
-     //   System.out.println(sum);
-
+             sum=pow(7,a)-cos(i);
+             y=y+sum;
+         }
+            System.out.printf("%10s%10f%10s%10f\n", "а=", a, "Сумма y=", y);
+            a=a+0.2;
+       }while ((0<=a)&&(a<=2));
     }
+
     private static void step2() {
+        double x = -5.5;
+        double a = 0;
+
+        do {
+            boolean non=false;
+            double beta=0;
+            double alpha =0;
+            if((-2<x/2)&&(x/2<=-1)){
+                beta=sin(x*x);
+                non=true;
+            }
+            if((-1<x/2)&&(x/2<0.2)){
+                beta=cos(x*x);
+                non=true;
+            }
+            if(x / 2 == 0.2){
+                beta=cos(x*x)/sin(x*x);
+                non=true;
+            }
+
+            if (non==true) {alpha=log10(abs(beta+2.74));
+                System.out.printf("%8s%4f%4s%10f\n", "При x/2=", x, "a=",alpha);
+            }
+            else {
+            System.out.printf("%8s%4f%18s\n", "При x/2=", x, " вычисления не определены");}
+            x=x+0.5;
+
+
+        }while((-6< x)&&(x<2));
     }
 
 }
